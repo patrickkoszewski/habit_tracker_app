c7d3507b50b9383bc437da9b3625c75176ad5563
Patrick <ledrap1992@gmail.com>
2022-12-12 04:30:08 +0100

    Organize Code, add habit name and progress

--- lib/util/habit_tile.dart
+++ lib/util/habit_tile.dart
@@ -6,7 +6,11 @@ class HabitTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.only(
+        left: 20.0,
+        right: 20.0,
+        top: 20.0,
+      ),
       child: Container(
         padding: EdgeInsets.all(20),
         decoration: BoxDecoration(
@@ -14,8 +18,17 @@ class HabitTile extends StatelessWidget {
           borderRadius: BorderRadius.circular(12),
         ),
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('Code'),
+            Column(
+              children: [
+                //habit name
+                Text('Code'),
+
+                //progres
+                Text('2:00 / 10 = 20%'),
+              ],
+            ),
             Icon(Icons.settings),
           ],
         ),
