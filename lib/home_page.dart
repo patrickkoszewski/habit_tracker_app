2df3f84845ac6dfde20a0c2deb727ca6f4f416ef
Patrick <ledrap1992@gmail.com>
2022-12-12 15:29:05 +0100

    add ListView.builder

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -17,27 +17,35 @@ class _HomePageState extends State<HomePage> {
     ['Kodowanie', false, 0, 120],
     ['Książka', false, 0, 60],
   ];
+
+  void habitStarted(int index) {}
+
+  void settingsOpened(int index) {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      appBar: AppBar(
-        backgroundColor: Colors.grey[900],
-        title: const Text('Konsekwencja to klucz.'),
-        centerTitle: false,
-      ),
-      body: Column(
-        children: [
-          HabitTile(
-            habitName: habitList[0][0],
-            onTap: () {},
-            settingsTapped: () {},
-            timeSpent: 4,
-            timeGoal: 12,
-            habitStarted: habitList[0][1],
-          ),
-        ],
-      ),
-    );
+        backgroundColor: Colors.grey[300],
+        appBar: AppBar(
+          backgroundColor: Colors.grey[900],
+          title: const Text('Konsekwencja to klucz.'),
+          centerTitle: false,
+        ),
+        body: ListView.builder(
+            itemCount: habitList.length,
+            itemBuilder: (((context, index) {
+              return HabitTile(
+                habitName: habitList[index][0],
+                onTap: () {
+                  habitStarted(index);
+                },
+                settingsTapped: () {
+                  settingsOpened(index);
+                },
+                habitStarted: habitList[index][1],
+                timeSpent: habitList[index][2],
+                timeGoal: habitList[index][3],
+              );
+            }))));
   }
 }
